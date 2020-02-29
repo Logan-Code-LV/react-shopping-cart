a879a6b5e2f37de720513f8cdfb9630199aa15cf
Logan Gayler <logangayler@gmail.com>
2020-02-28 16:34:59 -0800

    fridaycommit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,17 +2,23 @@ import React from "react"
 import { userProducts } from "../hooks"
 import { useItems } from "../hooks"
 import "../styles/Cart.css"
+import { FaShoppingCart } from "react-icons/fa"
 
-export default function() {
-  const { cart } = useItems()
+export default function(e) {
+  const { cart, toggle, isOpen, del } = useItems()
 
   return (
     <div className="cart">
+      {isOpen ? <h1>OPEN</h1> : <h1>CLOSED</h1>}
+      <button onClick={e => toggle()}>
+        <FaShoppingCart />
+      </button>
       {cart.map(item => (
         <div key={item.id}>
           <img className="cartimg" src={`/assets/${item.sku}_2.jpg`} />
           <p>{item.title}</p>
           <p>${item.price.toFixed(2)}</p>
+          <button onClick={e => del(item)}>X</button>
         </div>
       ))}
     </div>
--- src/redux/ducks/cart/actions.js
+++ src/redux/ducks/cart/actions.js
@@ -1,43 +1,55 @@
 // 1. imports
-import axios from "axios"
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
-
+import axios from "axios"
 // 2. action definitions
-const GET_ITEM = "cart/GET_ITEM"
-
+const TOGGLE_CART = "cart/TOGGLE_CART"
+const ADD_ITEM = "cart/ADD_ITEM"
+const DEL_ITEM = "cart/DEL_ITEM"
 // 3. initial state
 const initialState = {
-  cart: []
+  cart: [],
+  isOpen: false
 }
-
 // 4. reducer
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_ITEM:
-      return { ...state, cart: action.payload }
+    case ADD_ITEM:
+      return { ...state, cart: [...state.cart, action.payload] }
+    case TOGGLE_CART:
+      return { ...state, isOpen: !state.isOpen }
+    case DEL_ITEM:
+      return {
+        ...state,
+        cart: [...state.cart.filter(cart => cart !== action.payload)]
+      }
     default:
       return state
   }
 }
-
 // 5. action creators
-function getItems() {
+
+function addItem(product) {
   return dispatch => {
-    axios.get("/cart").then(resp => {
-      dispatch({
-        type: GET_ITEM,
-        payload: resp.data
-      })
+    dispatch({
+      type: ADD_ITEM,
+      payload: product
     })
   }
 }
 
-function addItem(product) {
+function delItem(product) {
   return dispatch => {
-    axios.post("/cart", product).then(resp => {
-      dispatch(getItems())
+    dispatch({
+      type: DEL_ITEM,
+      payload: product
     })
+  }
+}
+
+function toggleCart() {
+  return {
+    type: TOGGLE_CART
   }
 }
 
@@ -45,11 +57,78 @@ function addItem(product) {
 export function useItems() {
   const dispatch = useDispatch()
   const cart = useSelector(appState => appState.cartState.cart)
+  const isOpen = useSelector(appState => appState.cartState.isOpen)
   const add = product => dispatch(addItem(product))
-
+  const del = product => dispatch(delItem(product))
+  const toggle = () => dispatch(toggleCart())
+  //const del = product => dispatch(deleteItem(product))
   useEffect(() => {
-    dispatch(getItems())
+    console.log(cart)
   }, [])
-
-  return { cart, add }
+  return { cart, add, isOpen, toggle, del }
 }
+
+// // 1. imports
+// import axios from "axios"
+// import { useEffect } from "react"
+// import { useSelector, useDispatch } from "react-redux"
+
+// // 2. action definitions
+// const GET_ITEM = "cart/GET_ITEM"
+
+// // 3. initial state
+// const initialState = {
+//   cart: []
+// }
+
+// // 4. reducer
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case GET_ITEM:
+//       return { ...state, cart: action.payload }
+//     default:
+//       return state
+//   }
+// }
+
+// // 5. action creators
+// function getItems() {
+//   return dispatch => {
+//     axios.get("/cart").then(resp => {
+//       dispatch({
+//         type: GET_ITEM,
+//         payload: resp.data
+//       })
+//     })
+//   }
+// }
+
+// function addItem(product) {
+//   return dispatch => {
+//     axios.post("/cart", product).then(resp => {
+//       dispatch(getItems())
+//     })
+//   }
+// }
+
+// function deleteItem(product) {
+//   return dispatch => {
+//     axios.delete("/cart", product).then(resp => {
+//       dispatch(getItems())
+//     })
+//   }
+// }
+
+// // 6. custom hook
+// export function useItems() {
+//   const dispatch = useDispatch()
+//   const cart = useSelector(appState => appState.cartState.cart)
+//   const add = product => dispatch(addItem(product))
+//   const del = product => dispatch(deleteItem(product))
+
+//   useEffect(() => {
+//     dispatch(getItems())
+//   }, [])
+
+//   return { cart, add, del }
+// }
