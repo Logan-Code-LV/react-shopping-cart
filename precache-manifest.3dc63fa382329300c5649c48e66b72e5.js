d5a72ed7765ecf532cc5235e6e3dca535e3a800a
Logan-Code-LV <logangayler@gmail.com>
2020-05-14 10:56:05 -0700

    Updates

---
+++ precache-manifest.3dc63fa382329300c5649c48e66b72e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49f5647524ef5502aafbf25a94d472f",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "dc9ab3ca49989d4cb2f8",
+    "url": "/react-shopping-cart/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c928d07fc54e0c98e6d5",
+    "url": "/react-shopping-cart/static/css/main.e0453fbd.chunk.css"
+  },
+  {
+    "revision": "dc9ab3ca49989d4cb2f8",
+    "url": "/react-shopping-cart/static/js/2.45bb92d4.chunk.js"
+  },
+  {
+    "revision": "c928d07fc54e0c98e6d5",
+    "url": "/react-shopping-cart/static/js/main.a512cecb.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
